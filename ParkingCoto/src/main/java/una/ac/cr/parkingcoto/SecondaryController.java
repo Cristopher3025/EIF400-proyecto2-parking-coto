4bff3d1aa408f8a2206d30daa9d2e7ec964f78a2
Justin Rojas Jarquin <141199654+JustinRojasJarquin@users.noreply.github.com>
2026-09-15 17:47:22 -0600

    Implements Project

---
+++ ParkingCoto/src/main/java/una/ac/cr/parkingcoto/SecondaryController.java
@@ -0,0 +1,12 @@
+package una.ac.cr.parkingcoto;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
