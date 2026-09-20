e2bf875bb3c6ec52c802a8321ce0989a4f6ac0b5
Justin Rojas Jarquin <Justin Rojas>
2026-09-20 17:35:42 -0600

    Creando la estructura general del proyecto

--- ParkingCoto/src/main/java/una/ac/cr/parkingcoto/App.java
+++ ParkingCoto/src/main/java/una/ac/cr/parkingcoto/App.java
@@ -1,38 +1,41 @@
 package una.ac.cr.parkingcoto;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+
+        scene = new Scene(loadFXML("EntryView"), 1200, 750);
+
+        stage.setTitle("Parking Coto");
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+
+        FXMLLoader loader = new FXMLLoader(
+                App.class.getResource("/view/" + fxml + ".fxml")
+        );
+
+        return loader.load();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
